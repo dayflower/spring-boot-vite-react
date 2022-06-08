03f83372f3981deb28e5a5ac0de898645032fc78
dayflower <daydream.trippers@gmail.com>
2022-06-08 22:09:33 +0900

    Handle ConnectException for upstream (frontend)

--- src/main/kotlin/com/example/dayflower/sbvr/ProxyController.kt
+++ src/main/kotlin/com/example/dayflower/sbvr/ProxyController.kt
@@ -1,27 +1,33 @@
 package com.example.dayflower.sbvr
 
 import org.springframework.context.annotation.Profile
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
 import org.springframework.stereotype.Controller
+import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.ResourceAccessException
 import org.springframework.web.client.RestTemplate
+import java.net.ConnectException
 import java.net.URI
 import javax.servlet.http.HttpServletRequest
 
 @Profile("local")
 @Controller
 class ProxyController(
-    private val restTemplate: RestTemplate
+    private val restTemplate: RestTemplate,
 ) {
     companion object {
         private const val FRONTEND_HOST = "localhost"
         private const val FRONTEND_PORT = 3000
+        private val CONTENT_TYPE_JSON_HEADERS =
+            HttpHeaders.readOnlyHttpHeaders(
+                LinkedMultiValueMap<String, String>().apply {
+                    add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                }
+            )
     }
 
     @RequestMapping(name = "proxyRequest")
@@ -42,6 +48,15 @@ class ProxyController(
             ResponseEntity.status(ex.statusCode)
                 .headers(ex.responseHeaders)
                 .body(ex.responseBodyAsByteArray)
+        } catch (ex: ResourceAccessException) {
+            if (ex.rootCause is ConnectException) {
+                return ResponseEntity
+                    .status(HttpStatus.BAD_GATEWAY)
+                    .headers(CONTENT_TYPE_JSON_HEADERS)
+                    .body("""{ "status": 502, "error": "bad_gateway", "detail": "connect error" }""".toByteArray())
+            }
+
+            throw ex
         }
     }
 }
